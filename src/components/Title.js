ab191c545ddba4c04017b2f70c1b0d77cc416cfd
siddeshwar-kagatikar <siddeshwar2004@gmail.com>
2023-08-28 22:34:35 +0530

    learning git

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,6 +5,7 @@ import Modal from './Modal';
 export default function Title(props) {
   return (
     <div className='my-3'>
+      
      <Modal id={props.id} title={props.title} description={props.description}/>
     </div>
   )
